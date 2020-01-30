2f79a01d29f779e6ccc2fd03e64bf78580a8c47f
Christine Vinaviles <christine.vinaviles@gmail.com>
2020-01-30 13:21:39 +1100

    hmmm

--- puzzlesocweb/puzzlesocweb/wsgi.py
+++ puzzlesocweb/puzzlesocweb/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'puzzlesocweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'puzzlesocweb.puzzlesocweb.settings')
 
 application = get_wsgi_application()
